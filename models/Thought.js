a38f61991574ad395605faf0d6cca6d20dd4bb7f
TheWiseSailor <rudasilljay@gmail.com>
2023-08-23 13:26:33 -0400

    I think for the most part i competed the thoughts.js and if I need to make any other changes to it in the future i probably will

--- models/Thought.js
+++ models/Thought.js
@@ -3,3 +3,69 @@
 // String, required
 // Use the Reaction subdocument schema
 // Define a virtual for reactionCount
+// Import required libraries
+import mongoose from 'mongoose';
+import dateFormat from 'your-date-format-library'; // Replace with actual date format library
+
+// Destructure Schema from mongoose
+const { Schema } = mongoose;
+
+// Define Reaction subdocument schema
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+    unique: true, // Ensure unique reaction bodies
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: timestamp => dateFormat(timestamp),
+  },
+});
+
+// Define main Thought schema
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 10, // Increased minimum length for uniqueness
+    maxlength: 420, // Embracing the internet culture
+    unique: true, // Ensure unique thought texts
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: timestamp => dateFormat(timestamp),
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true, // Trim whitespace from usernames
+  },
+  reactions: [reactionSchema], // Include reaction schema
+  friends: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User', // Reference to User model
+  }],
+});
+
+// Define virtual property for friendCount
+thoughtSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+// Compile the Thought model
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+// Export the model
+export default Thought;
--- models/User.js
+++
@@ -1,5 +0,0 @@
-// String, unique, required, trimmed
-// String, required, unique, match valid email address
-// Array of _id values referencing the Thought model
-// Array of _id values referencing the User model (self-reference)
-// Define a virtual for friendCount
