333e91a30da2363abd25f27475c389de8cb1e54b
TheWiseSailor <rudasilljay@gmail.com>
2023-08-24 18:22:45 -0400

    debugging, and read me made

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,94 +1,109 @@
-// Import necessary models
-// Define controller methods for CRUD operations on thoughts
-// Retrieve all thoughts from the database
-// Retrieve a single thought by its _id
-// Create a new thought and update the associated user's thoughts array
-// Update a thought by its _id
-// Delete a thought by its _id and also remove its reference from the user's thoughts array
-// Create a new reaction and push it to a thought's reactions array
-// Delete a reaction from a thought's reactions array
-// Importing User and Thought models for database operations
-const User = require("../models/User");
 const Thought = require("../models/Thought");
-// Controller object to handle user-related operations
-const userController = {
-  // Get all users with their thoughts and friends
-  getAllUsers(req, res) {
-    User.find({})
-      .populate("thoughts") // Populate user's thoughts
-      .populate("friends") // Populate user's friends
-      .then((userData) => res.json(userData))
-      .catch((err) => res.status(500).json(err));
-  },
-  // Get a single user by ID with their thoughts and friends
-  getSingleUser(req, res) {
-    const { id } = req.params;
-    User.findOne({ _id: id })
-      .populate("thoughts") // Populate user's thoughts
-      .populate("friends") // Populate user's friends
-      .then((userData) => {
-        if (!userData) {
-          res.status(404).json({ message: "User not found" });
-          return;
-        }
-        res.json(userData);
-      })
-      .catch((err) => res.status(500).json(err));
-  },
-  // Create a new user
-  createUser(req, res) {
-    const { username, email } = req.body;
-    User.create({ username, email })
-      .then((userData) => res.json(userData))
-      .catch((err) => res.status(400).json(err));
-  },
-  // Update user's information
-  updateUser(req, res) {
-    const { id } = req.params;
-    const { username, email } = req.body;
-    User.findOneAndUpdate({ _id: id }, { username, email }, { new: true })
-      .then((userData) => {
-        if (!userData) {
-          res.status(404).json({ message: "User not found" });
-          return;
-        }
-        res.json(userData);
-      })
-      .catch((err) => res.status(400).json(err));
-  },
-  // Delete a user and their associated thoughts
-  deleteUser(req, res) {
-    const { id } = req.params;
-    User.findOneAndDelete({ _id: id })
-      .then((userData) => {
-        if (!userData) {
-          res.status(404).json({ message: "User not found" });
-          return;
-        }
-        // Remove user's thoughts
-        return Thought.deleteMany({ username: userData.username });
-      })
-      .then(() => res.json({ message: "User and associated thoughts deleted" }))
+const User = require("../models/User");
+
+const thoughtController = {
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .then((thoughtData) => res.json(thoughtData))
       .catch((err) => res.status(500).json(err));
   },
 
-  // Remove a friend from the user's friend list
-  removeFriend(req, res) {
-    const { userId, friendId } = req.params;
-    User.findByIdAndUpdate(
-      userId,
-      { $pull: { friends: friendId } }, // Remove friend from the set of friends
-      { new: true }
-    )
-      .populate("friends") // Populate user's friends
-      .then((userData) => {
-        if (!userData) {
-          res.status(404).json({ message: "User not found" });
+  getSingleThought(req, res) {
+    const { id } = req.params;
+    Thought.findOne({ _id: id })
+      .then((thoughtData) => {
+        if (!thoughtData) {
+          res.status(404).json({ message: "Thought not found" });
           return;
         }
-        res.json(userData);
+        res.json(thoughtData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  createThought(req, res) {
+    const { thoughtText, username } = req.body;
+    Thought.create({ thoughtText, username })
+      .then((thoughtData) => {
+        return User.findOneAndUpdate(
+          { username },
+          { $push: { thoughts: thoughtData._id } },
+          { new: true }
+        );
+      })
+      .then((userData) => res.json(userData))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  updateThought(req, res) {
+    const { id } = req.params;
+    const { thoughtText } = req.body;
+    Thought.findOneAndUpdate({ _id: id }, { thoughtText }, { new: true })
+      .then((thoughtData) => {
+        if (!thoughtData) {
+          res.status(404).json({ message: "Thought not found" });
+          return;
+        }
+        res.json(thoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  deleteThought(req, res) {
+    const { id } = req.params;
+    Thought.findOneAndDelete({ _id: id })
+      .then((thoughtData) => {
+        if (!thoughtData) {
+          res.status(404).json({ message: "Thought not found" });
+          return;
+        }
+        // Remove thought's reactions
+        return User.updateMany(
+          { thoughts: id },
+          { $pull: { thoughts: id } },
+          { multi: true }
+        );
+      })
+      .then(() =>
+        res.json({ message: "Thought and associated reactions deleted" })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  addReaction(req, res) {
+    const { thoughtId } = req.params;
+    const { reactionBody, username } = req.body;
+    Thought.findOneAndUpdate(
+      { _id: thoughtId },
+      { $push: { reactions: { reactionBody, username } } },
+      { new: true }
+    )
+      .then((thoughtData) => {
+        if (!thoughtData) {
+          res.status(404).json({ message: "Thought not found" });
+          return;
+        }
+        res.json(thoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  removeReaction(req, res) {
+    const { thoughtId, reactionId } = req.params;
+    Thought.findOneAndUpdate(
+      { _id: thoughtId },
+      { $pull: { reactions: { reactionId } } },
+      { new: true }
+    )
+      .then((thoughtData) => {
+        if (!thoughtData) {
+          res.status(404).json({ message: "Thought not found" });
+          return;
+        }
+        res.json(thoughtData);
       })
       .catch((err) => res.status(400).json(err));
   },
 };
-module.exports = userController;
+
+module.exports = thoughtController;
--- readme.md
+++ readme.md
@@ -1,10 +1,102 @@
-# Social
+# Social Network API
 
-## sources:
+This is the Social Network API, welcome! For a social network online application where users may post their opinions, respond to friends' thoughts, and maintain their friend lists, this project offers an API.
+
+## Table of Contents
+
+- [Introduction](#social-network-api)
+- [Getting Started](#getting-started)
+  - [Installation](#installation)
+  - [Environment Setup](#environment-setup)
+- [Models](#models)
+  - [User Model](#user-model)
+  - [Thought Model](#thought-model)
+  - [Reaction Schema](#reaction-schema)
+- [API Routes](#api-routes)
+  - [Users](#users)
+  - [Thoughts](#thoughts)
+- [Usage](#usage)
+- [Walkthrough Video](#walkthrough-video)
+- [Deployment](#deployment)
+- [Credits](#credits)
+- [License](#license)
+
+## Getting Started
+
+### Installation
+
+1. Clone this repository to your local machine.
+2. Install the required dependencies using `npm install`.
+
+### Environment Setup
+
+1. Create a `.env` file in the root directory.
+2. Set up your environment variables in the `.env` file. For example:
+   ```env
+   MONGODB_URI=mongodb://localhost:27017/Socialnetwork
+   PORT=3001
+   ```
+
+## Models
+
+### User Model
+
+- username: String (Unique, Required, Trimmed)
+- email: String (Required, Unique, Must match a valid email address)
+- thoughts: Array of \_id values referencing the Thought model
+- friends: Array of \_id values referencing the User model (self-reference)
+- Virtual: friendCount to retrieve the length of the user's friends array
+
+The User model represents a user in the social network. It includes fields for the user's username, email, thoughts they have posted, and their friends list.
+
+### Thought Model
+
+The Thought model represents a thought or post shared by a user in the social network. It includes fields for the thought's text content, creation timestamp, the username of the user who posted it, and an array of reactions.
+
+### Reaction Schema
+
+The Reaction schema is used to define the structure of individual reactions within a thought. Each reaction includes a unique reactionId, the reaction's body or content, the username of the user who created the reaction, and a creation timestamp.
+
+## API Routes
+
+### Users
+
+- GET /api/users: Get all users
+- GET /api/users/:id: Get a single user by id and populate thought and friend data
+- POST /api/users: Create a new user
+- PUT /api/users/:id: Update a user by id
+- DELETE /api/users/:id: Delete a user by id
+- POST /api/users/:userId/friends/:friendId: Add a new friend to a user's friend list
+- DELETE /api/users/:userId/friends/:friendId: Remove a friend from a user's friend list
+
+### Thoughts
+
+- GET /api/thoughts: Get all thoughts
+- GET /api/thoughts/:id: Get a single thought by id
+- POST /api/thoughts: Create a new thought
+- PUT /api/thoughts/:id: Update a thought by id
+- DELETE /api/thoughts/:id: Delete a thought by id
+- POST /api/thoughts/:thoughtId/reactions: Create a reaction for a thought
+- DELETE /api/thoughts/:thoughtId/reactions/:reactionId: Remove a reaction from a thought
+
+## Usage
+
+1. Start the API server using `npm start`.
+2. Use tools like Postman or Insomnia to send requests to the API routes.
+
+## Walkthrough Video
+
+[Link to Your Walkthrough Video](#)
+
+## Deployment
+
+To deploy the Social Network API to Heroku (or other platforms), follow the deployment steps specific to the platform.
+
+## Credits
 
 https://developer.mozilla.org/en-US/ <br/>
 https://stackoverflow.com/ <br/>
 https://www.youtube.com/ <br/>
 https://www.w3schools.com/ <br/>
 https://mongoosejs.com/docs/ <br/>
-Donna Thompson 
+Donna Thompson
