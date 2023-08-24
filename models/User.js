8c4b11964c53ac1859867bb12a99da51d29aaa3c
TheWiseSailor <rudasilljay@gmail.com>
2023-08-24 18:49:59 -0400

    debug

--- models/User.js
+++ models/User.js
@@ -1,48 +1,47 @@
-// Import the mongoose library
-const mongoose = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-// Destructure the Schema object from mongoose
-const { Schema } = mongoose;
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Please enter a valid email address"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    // Virtuals to be included with our response
+    toJSON: {
+      virtuals: true,
+    },
+    // Exclude the default '_id' field from responses
+    id: false,
+  }
+);
 
-// Create a new Schema for the 'User' model
-const userSchema = new Schema({
-  // Define the 'username' field
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true // Remove extra whitespace
-  },
-  // Define the 'email' field
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Please enter a valid email'] // Validate email format
-  },
-  // Define the 'thoughts' field as an array of ObjectIds referencing 'Thought' model
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Thought' // Referencing 'Thought' model
-    }
-  ],
-  // Define the 'friends' field as an array of ObjectIds referencing 'User' model
-  friends: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'User' // Referencing 'User' model for friendship connections
-    }
-  ]
+// Define the virtual for friendCount
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
-// Create a virtual property 'friendCount' for the 'User' model
-userSchema.virtual('friendCount').get(function() {
-  return this.friends.length; // Calculate and return the count of friends
-});
+const User = model("User", userSchema);
 
-// Create the 'User' model using the defined schema
-const User = mongoose.model('User', userSchema);
-
-// Export the 'User' model for external use
 module.exports = User;
